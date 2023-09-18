9f9944d2faaf0f989d8dcb35ad7ded85942111c2
ishihara-fumiya <ishihara@corp-jjs.com>
2023-09-18 20:56:05 +0900

    正誤判定、とりあえずロジック自体は動いた

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -1,50 +1,20 @@
-function sample() {
-  document.getElementById("area1").innerText = "変更しました";
+function submitAnswer() {
+  const userAnswer = document.getElementById("answer").value;
+  fetch("/check-answer", {
+    method: "POST", // 送信
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ answer: userAnswer }),
+  })
+    .then((response) => response.json()) // 受信?
+    .then((data) => {
+      // 正誤判定
+      const resultDiv = document.getElementById("result");
+      if (data.isCorrect) {
+        resultDiv.textContent = "正解";
+      } else {
+        resultDiv.textContent = "不正解";
+      }
+    });
 }
-function demoTest() {
-  alert("demoTest called.");
-}
-
-// document.getElementById("answer").addEventListener("click", function () {
-//   const userAnswer = document.getElementById("answer").value;
-
-//   fetch("/check-answer", {
-//     method: "POST", // 送信
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ answer: userAnswer }),
-//   })
-//     .then((response) => response.json()) // 受信?
-//     .then((data) => {
-//       // 正誤判定
-//       const resultDiv = document.getElementById("result");
-//       if (data.isCorrect) {
-//         resultDiv.textContent = "正解";
-//       } else {
-//         resultDiv.textContent = "不正解";
-//       }
-//     });
-// });
-
-// function checkAnswer() {
-//   const userAnswer = document.getElementById("answer").value;
-
-//   fetch("/check-answer", {
-//     method: "POST", // 送信
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ answer: userAnswer }),
-//   })
-//     .then((response) => response.json()) // 受信?
-//     .then((data) => {
-//       // 正誤判定
-//       const resultDiv = document.getElementById("result");
-//       if (data.isCorrect) {
-//         resultDiv.textContent = "正解";
-//       } else {
-//         resultDiv.textContent = "不正解";
-//       }
-//     });
-// }
