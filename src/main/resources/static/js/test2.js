6d7013cbad85f637ae1f88934d0749020577e943
ishihara-plasticity <fishihara41@gmail.com>
2023-10-18 16:59:19 +0900

    Save current changes

--- src/main/resources/static/js/test2.js
+++ src/main/resources/static/js/test2.js
@@ -1,14 +1,16 @@
 "use strict";
-var User = /** @class */ (function () {
-    function User(familyName, givenName, age) {
-        this.age = age;
-        this.familyName = familyName;
-        this.givenName = givenName;
-    }
-    return User;
-}());
-var user = new User('海老原', '賢次', 44); // 名前と年齢は適当に
-var contentsElem = document.getElementById('contents');
-if (!!contentsElem) {
-    contentsElem.innerText = "".concat(user.familyName, " ").concat(user.givenName);
-}
+// class User {
+//   public age: number;
+//   public familyName: string;
+//   public givenName: string;
+//   constructor (familyName: string, givenName: string, age: number) {
+//       this.age = age;
+//       this.familyName = familyName;
+//       this.givenName = givenName;
+//   }
+// }
+// const user = new User('海老原', '賢次', 44); // 名前と年齢は適当に
+// const contentsElem = document.getElementById('contents');
+// if(!!contentsElem) {
+//   contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+// }
