bd9be65d96cce70f317e85bee679691358644516
ishihara-plasticity <fishihara41@gmail.com>
2023-10-21 14:46:28 +0900

    デッドコードを削除

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -14,35 +14,4 @@ function submitAnswer() {
   const newHistoryItem = document.createElement("div");
   newHistoryItem.innerHTML = `<strong>${answerSubmitNumber}回目の回答</strong> ${userAnswer}`;
   historyDiv.appendChild(newHistoryItem);
-
-  // 正解かどうかをチェック（仮実装）
-  const isCorrect = true; // 本当はサーバから結果を取得
-
-  // 正解なら穴に答えを埋める
-  if (isCorrect && selectedWordIndex !== -1) {
-    const sentenceDiv = document.getElementById("sentence");
-    const words = sentenceDiv.innerText.split(" ");
-    words[selectedWordIndex] = userAnswer;
-    sentenceDiv.innerHTML = words
-      .map((word, i) => `<span onclick='selectWord(${i})'>${word}</span>`)
-      .join(" ");
-  }
-
-  fetch("/check-answer", {
-    method: "POST", // 送信
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ answer: userAnswer }),
-  })
-    .then((response) => response.json()) // 受信?
-    .then((data) => {
-      // 正誤判定
-      const resultDiv = document.getElementById("result");
-      if (data.isCorrect) {
-        resultDiv.textContent = "正解";
-      } else {
-        resultDiv.textContent = "不正解";
-      }
-    });
 }
